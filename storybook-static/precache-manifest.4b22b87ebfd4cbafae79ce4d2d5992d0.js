f36fd34427436be9f0b80ec1c0efc6aa12d38cb3
Lac <laclys@126.com>
2020-02-01 23:33:29 +0800

    build-storybook

---
+++ storybook-static/precache-manifest.4b22b87ebfd4cbafae79ce4d2d5992d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5fa7c043bb398c0aab0d2d49f92e9f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a6d1ed6ea2c734559322.bundle.js"
+  },
+  {
+    "url": "runtime~main.a6d1ed6ea2c734559322.bundle.js"
+  },
+  {
+    "revision": "344ae036b6935e998e52",
+    "url": "static/css/main.1974dbce.chunk.css"
+  },
+  {
+    "url": "vendors~main.a6d1ed6ea2c734559322.bundle.js"
+  },
+  {
+    "url": "vendors~main.a6d1ed6ea2c734559322.bundle.js.LICENSE.txt"
+  }
+]);
