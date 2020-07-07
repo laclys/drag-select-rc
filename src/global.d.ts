59a2c27577c52f4105336e8565e20f802b8ed598
Lac <laclys@126.com>
2020-07-07 17:46:43 +0800

    rebuild

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,7 +1,7 @@
 
 declare namespace JSX {
   interface IntrinsicElements {
-    td: React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & {
+    td: React.DetailedHTMLProps<React.AllHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & {
       disabled?: boolean;
     };
   }
