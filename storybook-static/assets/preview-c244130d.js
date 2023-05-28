df3f30eb6d66fe5b87e3a0fb4ee139437c52ba34
Lac <laclys@126.com>
2023-05-28 20:38:13 +0800

    del: storybook-static dir

--- storybook-static/assets/preview-c244130d.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-2b66a8f1.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-b6ec6c61.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-b6ec6c61.js","./iframe-2b66a8f1.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-c39e4407.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-c244130d.js.map
